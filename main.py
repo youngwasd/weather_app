c6492caf21bfc3b992d87abf7bc7fdc79b0472c9
youngwasd <youngk126621@gmail.com>
2024-02-16 18:22:51 -0800

    added front end :)

--- main.py
+++ main.py
@@ -2,28 +2,33 @@ import datetime as dt
 import requests
 from dotenv import load_dotenv
 import os
+from flask import Flask, render_template, request
 
 load_dotenv('.env')
 BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?"
 API_KEY = os.getenv('API_KEY')
-CITY = input("Enter city: ")
+app = Flask(__name__)
 
+# back end
 def kelToCF(kelvin):
     cels = kelvin - 273.15
     fah = cels * (9 / 5) + 32
     return cels, fah
 
-url = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+def valid_city(city):
+    url = f"{BASE_URL}q={city}&appid={API_KEY}"
+    response = requests.get(url).json()
+    if response['cod'] == '404':
+        return False
+    else:
+        return True
 
-#prettyResponse = json.dumps(requests.get(url).json(), indent=4)
-response = requests.get(url).json()
-
-if response['cod'] == '404':
-    print("No City Found.")
-else:
-    # stupid = json.dumps(response['list'][0]['wind']['speed'], indent=4)
-    # print(stupid)
-
+def weather(city):
+    url = f"{BASE_URL}q={city}&appid={API_KEY}"
+    
+    # prettyResponse = json.dumps(requests.get(url).json(), indent=4)
+    response = requests.get(url).json()
+    
     temp_kelvin = response['list'][0]['main']['temp']
     temp_cel, temp_fah = kelToCF(temp_kelvin)
 
@@ -39,10 +44,30 @@ else:
 
     wind_spd = response['list'][0]['wind']['speed']
 
-    print(f"Temperature in {CITY}: {temp_cel:.2f}°C or {temp_fah:.2f}°F")
-    print(f"Temperature in {CITY}: feels like: {feels_c:.2f}°C or feels like: {feels_f:.2f}°F")
-    print(f"Humidity in {CITY}: {humidity}%")
-    print(f"Wind Speed in {CITY}: {wind_spd}m/s")
-    print(f"General Weather Description in {CITY}: {desc}")
-    print(f"Sun rises in {CITY} at {sunrise} local time")
-    print(f"Sun sets in {CITY} at {sunset} local time")
+    # print(f"Temperature in {CITY}: {temp_cel:.2f}°C or {temp_fah:.2f}°F")
+    # print(f"Temperature in {CITY}: feels like: {feels_c:.2f}°C or feels like: {feels_f:.2f}°F")
+    # print(f"Humidity in {CITY}: {humidity}%")
+    # print(f"Wind Speed in {CITY}: {wind_spd}m/s")
+    # print(f"General Weather Description in {CITY}: {desc}")
+    # print(f"Sun rises in {CITY} at {sunrise} local time")
+    # print(f"Sun sets in {CITY} at {sunset} local time")
+
+    return city, temp_cel, temp_fah, desc, sunrise, sunset
+
+# front end logic
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city = request.form['input']
+        if valid_city(city):
+            city, tempC, tempF, dsc, sunr, suns = weather(city)
+            tempC_format = f"{tempC:.2f}"
+            tempF_format = f"{tempF:.2f}"
+            return render_template("index.html", city=city, tempC=tempC_format, tempF=tempF_format, dsc=dsc, sunr=sunr, suns=suns)
+        else:
+            return render_template("index.html", error="City Not Found.")
+    else:
+        return render_template("index.html", city="", tempC="", tempF="", dsc="", sunr="", suns="")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
